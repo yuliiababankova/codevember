244bd54b8b1c1a00f9a22d4efda71078ff655876
jbabankova <jbabankova@geeksforless.net>
2019-11-12 20:11:37 +0200

    second

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,22 +1,31 @@
 package base;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 public class BaseTests {
     private WebDriver driver;
+    protected HomePage homePage;
 
+
+    @BeforeClass
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
 
-       // driver.quit();
+        homePage = new HomePage(driver);
     }
 
-    public void main(String args[]){
-        BaseTests test = new BaseTests();
-        test.setUp();
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
     }
 }
