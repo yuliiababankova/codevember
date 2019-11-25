33b7bfdb9db721de04d2526db408f0d9cdfac1a7
jbabankova <jbabankova@geeksforless.net>
2019-11-25 16:12:21 +0200

    Dropdown

--- src/main/java/pages/DropdownPage.java
+++ src/main/java/pages/DropdownPage.java
@@ -3,13 +3,16 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class DropdownPage {
+public class DropdownPage  {
     private WebDriver driver;
+    public WebDriverWait wait;
     private By dropdown = By.id("dropdown");
 
 
@@ -30,5 +33,28 @@ public class DropdownPage {
     private Select findDropDownElenent(){
         return new Select(driver.findElement(dropdown));
     }
+    public void makePause(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public WebElement clickWhenReady(WebElement element) {
+        makePause(200);
+        waitForVisible(element);
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+        return element;
+    }
+
+    public WebElement waitForVisible(WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element));
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        js.executeScript("arguments[0].style.border='3px dashed #ffe207'", element);
+        return element;
+    }
+
+
 
 }
--- src/test/java/dropdown/DropdownTests.java
+++ src/test/java/dropdown/DropdownTests.java
@@ -7,15 +7,21 @@ import pages.DropdownPage;
 import java.util.List;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class DropdownTests extends BaseTests {
 
     @Test
     public void testSelectOption(){
-  /*      DropdownPage dropDownPage = homePage.clickDropDown();
-        dropDownPage.selectFormDropDown("Option 1");
-        var selectedOptions = dropDownPage.getSelectOptions();
+        DropdownPage dropDownPage = homePage.clickDropDown();
+        String option = "Option 1";
+        dropDownPage.selectFormDropDown(option);
+
+        List selectedOptions = dropDownPage.getSelectOptions();
         assertEquals(selectedOptions.size(), 1, "Incorrect number of selections");
-*/
+        assertTrue(selectedOptions.contains(option), "Option not selected");
+
+
+
     }
 }
