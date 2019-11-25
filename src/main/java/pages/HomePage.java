7ae99fc60af391a7521a48f69543be3cfd920870
jbabankova <jbabankova@geeksforless.net>
2019-11-25 20:24:16 +0200

    key

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HomePage {
     private WebDriver driver;
@@ -30,7 +31,11 @@ public class HomePage {
     public HoversPage clickHovers(){
         clickLink("Hovers");
         return new HoversPage(driver);
+    }
 
+    public KeyPressesPage clickKeyPresses(){
+        clickLink("Key Presses");
+        return new KeyPressesPage(driver);
     }
 
     public WysiwygEditorPage clickWysiwygEditor(){
